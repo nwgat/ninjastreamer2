82def378d4724e4e07eb25bf7b61ad3e350e7283
nwgat <github@nwgat.net>
2018-04-19 22:10:51 +0200

    Update ninjastreamer.sh

--- ninjastreamer.sh
+++ ninjastreamer.sh
@@ -19,8 +19,9 @@ apt-get install -y ffmpeg
 # install nginx-rtmp
 wget -q https://awesome.nwgat.ninja/nginx-rtmp/nginx-common_1.10.1-0ubuntu1.2_all.deb 
 wget -q https://awesome.nwgat.ninja/nginx-rtmp/nginx-full_1.10.1-0ubuntu1.2_amd64.deb
-dpkg -i nginx-full_1.10.1-0ubuntu1.2_amd64.deb nginx-common_1.10.1-0ubuntu1.2_all.deb
-apt-get install -f -y
+dpkg -i nginx-full_1.10.1-0ubuntu1.2_amd64.deb nginx-common_1.10.1-0ubuntu1.2_all.deb >> /dev/null
+apt-get install -f -y >> /dev/null
+
 # install nginx-rtmp-keyauth
 cp nginx-rtmp-keyauth /usr/local/bin/nginx-rtmp-keyauth
 chmod +x /usr/local/bin/nginx-rtmp-keyauth
@@ -35,14 +36,18 @@ cp -R html /var/www/
 systemctl daemon-reload
 systemctl enable nginx nginx-rtmp-keyauth
 
-systemctl start nginx nginx-rtmp-keyauth
-systemctl status nginx nginx-rtmp-keyauth
+systemctl start nginx 
+sleep 5
+systemctl start nginx-rtmp-keyauth
 
-#firewalls
-ufw allow 1935
-ufw allow 8080
-ufw allow 80
+# firewalls
+ufw allow 1935 >> /dev/null
+ufw allow 8080 >> /dev/null
+ufw allow 80 >> /dev/null
 
+echo ""
 # Details
 echo "Ninja Player & Details" http://$iphost
 echo "RTMP Password:" $pw
+echo ""
+echo ""
